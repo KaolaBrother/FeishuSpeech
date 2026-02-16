e4356e8678db37219d1c03b849fe9efabf70a768
KaolaBrother <yanleichen@hotmail.com>
2026-02-16 20:34:21 +0800

    Initial Commit

---
+++ FeishuSpeech/FeishuSpeechApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeishuSpeechApp.swift
+//  FeishuSpeech
+//
+//  Created by 陈焱磊 on 2026/2/16.
+//
+
+import SwiftUI
+
+@main
+struct FeishuSpeechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
